4891831c12f251018289fdc3ec24f00b09348faf
Joey Geralnik <jgeralnik@gmail.com>
2011-08-19 15:59:00 +0300

    Added a second cache for the minmax function itself

--- minmax.py
+++ minmax.py
@@ -3,16 +3,29 @@
 
 ENDGAME = 10000
 
-myCache = {}
-hits = 0
-misses = 0
+scoreCache = {}
+#Score cache keys are states.
+#The items are the score
+
+minmaxCache = {}
+#Minmax cache keys are player,state
+#The items are best score, state, required depth
+#Items only go into the cache if:
+#  a)Guaranteed win/loss
+#  b)Researched to max depth
+
+scoreHits = 0
+scoreMisses = 0
+minmaxHits = 0
+minmaxMisses = 0
+
 def TTTscore(state):
-    global hits, misses
+    global scoreHits, scoreMisses
     str_state = str(state)
-    if str_state in myCache:
-        hits+=1
-        return myCache[str_state]
-    misses+=1
+    if str_state in scoreCache:
+        scoreHits+=1
+        return scoreCache[str_state]
+    scoreMisses+=1
     wins = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
     s = 0
     def same(i,j):
@@ -25,7 +38,7 @@ def TTTscore(state):
     
     v = win(state)
     if v:
-        myCache[str_state] = ENDGAME*v
+        scoreCache[str_state] = ENDGAME*v
         return ENDGAME*v
     
     for i in range(len(state)):
@@ -38,7 +51,7 @@ def TTTscore(state):
                 s-=1
             state[i]=0
     
-#    myCache[str_state] = s
+    scoreCache[str_state] = s
     return s
 
 def TTTgetNext(player, state):
@@ -59,15 +72,22 @@ def TTTprintBoard(state):
 
 
 def minmax(player, state, max_level=7, level=0, tree=False, getNext = TTTgetNext, score = TTTscore):
+    global minmaxHits, minmaxMisses
+    if (player, str(state)) in minmaxCache:
+        minmaxHits+=1
+        return minmaxCache[(player, str(state))]
+    minmaxMisses+=1
     s = score(state)
     if abs(s)==ENDGAME: #Game over, stop!
         if tree:
+            minmaxCache[(player, str(state))] = (s, [], level)
             return (s, [], level)
         else:
+            minmaxCache[(player, str(state))] = (s, None, level)
             return (s,None, level)
 
     states = getNext(player, state)
-    if states == []:
+    if states == []: #No more moves, stop!
         if tree:
             return (s, [], level)
         else:
@@ -102,9 +122,11 @@ def minmax(player, state, max_level=7, level=0, tree=False, getNext = TTTgetNext
                     max_level = needed_depth
     if level==0 and tree:
         best.reverse()
+    if level==0 or abs(bestScore) == ENDGAME:
+        minmaxCache[(player, str(state))] = bestScore, best, needed_depth 
     return bestScore,best, needed_depth
 
-def play(getNext = TTTgetNext, score = TTTscore, printBoard = TTTprintBoard):
+def playAgainst(getNext = TTTgetNext, score = TTTscore, printBoard = TTTprintBoard):
     state = [0]*9
     computer = -1
     player = 1
@@ -118,10 +140,13 @@ def play(getNext = TTTgetNext, score = TTTscore, printBoard = TTTprintBoard):
             state[move]=player
             printBoard(state)
 
-
-minmax(1,[0]*9,9)
-minmax(1,[0]*9,9)
-minmax(1,[0]*9,9)
-
-print hits,misses
-
+def play(state = [0]*9, getNext = TTTgetNext, score = TTTscore, printBoard = TTTprintBoard):
+    p1 = -1
+    p2 =  1
+    printBoard(state)
+    while abs(score(state))!=ENDGAME and getNext(p1,state)!=[]:
+        state = minmax(p1, state, 9, getNext=getNext, score=score)[1]
+        printBoard(state)
+        if abs(score(state))!=ENDGAME and getNext(p2,state)!=[]:
+            state = minmax(p2, state, 9, getNext=getNext, score=score)[1]
+            printBoard(state)
